350d6f0ecd8429020e74d50a052ba52eb496db98
CalClemmer <cal1296@yahoo.com>
2021-08-27 15:10:26 -0700

    Basic functionality established

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const ejsLayouts = require('express-ejs-layouts');
+
+const PORT = process.env.PORT || 8000;
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+
+
+app.get('/', function(req, res) {
+    res.render('home');
+  });
+
+app.get('/animals', function(req, res) {
+    res.render('animals', {title: 'Favorite Animals', animals: ['sand crab', 'corny joke dog']})
+});
+
+app.get('/foods', function(req, res) {
+    res.render('foods', {title: 'Favorite Foods', foods: ['ice cream', 'sushi']})
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on PORT', PORT);
+});
+
+
